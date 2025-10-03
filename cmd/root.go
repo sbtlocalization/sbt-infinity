40cf5f585064c3d205828a2695fbdd0a3bffd4f4
GooRoo <sergey.olendarenko@gmail.com>
2025-10-03 16:03:23 +0200

    Add an ability to enable profiling

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ package cmd
 
 import (
 	"os"
+	"runtime/pprof"
 
 	"github.com/sbtlocalization/sbt-infinity/cmd/dialog"
 	"github.com/sbtlocalization/sbt-infinity/cmd/text"
@@ -19,7 +20,13 @@ var rootCmd = &cobra.Command{
 	Long: `SBT Infinity Tools is a collection of utilities designed to assist with the localization and 
 modification of games based on the Infinity Engine, such as Baldur's Gate, Baldur's Gate II, 
 and Planescape: Torment.`,
+	Version: "0.1.0",
+
+	PersistentPreRunE:  startProfiling,
+	PersistentPostRunE: stopProfiling,
 }
+
+var profilingFile *os.File
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -33,4 +40,26 @@ func Execute() {
 func init() {
 	rootCmd.AddCommand(dialog.NewCommand())
 	rootCmd.AddCommand(text.NewCommand())
+
+	rootCmd.PersistentFlags().BoolP("profile", "p", false, "Enable profiling")
+	rootCmd.PersistentFlags().MarkHidden("profile")
+}
+
+func startProfiling(cmd *cobra.Command, args []string) error {
+	profile, _ := cmd.Flags().GetBool("profile")
+	if profile {
+		var err error
+		profilingFile, err = os.Create("cpu.pprof")
+		if err != nil {
+			return err
+		}
+		pprof.StartCPUProfile(profilingFile)
+	}
+	return nil
+}
+
+func stopProfiling(cmd *cobra.Command, args []string) error {
+	pprof.StopCPUProfile()
+	profilingFile.Close()
+	return nil
 }
