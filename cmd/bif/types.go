3c98efdf22bb0842c040ba8f699b4e577a35c95a
GooRoo <sergey.olendarenko@gmail.com>
2025-10-06 15:59:20 +0200

    Sort `bif types` output + add `--json`

--- cmd/bif/types.go
+++ cmd/bif/types.go
@@ -6,7 +6,9 @@
 package bif
 
 import (
+	"encoding/json"
 	"fmt"
+	"slices"
 
 	"github.com/sbtlocalization/sbt-infinity/fs"
 	"github.com/spf13/cobra"
@@ -14,8 +16,8 @@ import (
 
 func NewTypesCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "types",
-		Short:   "Lists all known resourse types",
+		Use:   "types",
+		Short: "Lists all known resourse types",
 		Long: `Shows all types which can be passed to type filter.
 https://gibberlings3.github.io/iesdp/file_formats/general.htm can be visited
 for types description.`,
@@ -24,12 +26,39 @@ for types description.`,
 		Args: cobra.MaximumNArgs(0),
 	}
 
+	cmd.Flags().BoolP("json", "j", false, "Output in JSON format")
+
 	return cmd
 }
 
 // `bif types` handler
 func runTypesBif(cmd *cobra.Command, args []string) {
-	for number, name := range *fs.GetAllTypes() {
-		fmt.Printf("%5s %6d\n", name, number)
+	jsonOutput, _ := cmd.Flags().GetBool("json")
+
+	types := *fs.GetAllTypes()
+	var exts []string
+	for _, name := range types {
+		exts = append(exts, name)
 	}
+	slices.Sort(exts)
+
+	if jsonOutput {
+		for _, e := range exts {
+			entry := typeEntry{
+				Type:  e,
+				Value: int(fs.FileTypeFromExtension(e)),
+			}
+			data, _ := json.Marshal(entry)
+			fmt.Println(string(data))
+		}
+	} else {
+		for _, e := range exts {
+			fmt.Printf("%5s %6d\n", e, fs.FileTypeFromExtension(e))
+		}
+	}
+}
+
+type typeEntry struct {
+	Type  string `json:"type"`
+	Value int    `json:"value"`
 }
